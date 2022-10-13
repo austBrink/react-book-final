0a01c7e64df05fb40405fb97cb8dcab6684f763b
austBrink <austbrink@gmail.com>
2022-10-13 05:42:19 -0500

    use state object instead of two states

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -5,11 +5,34 @@ import Quill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 const PostForm = (props) => {
+
+  // decided on one object for form values....
+  const [ postData, setPostData ] = useState({
+    title: '',
+    content: '',
+  });
+
+  // ...that way I could be general with this reusable function...AND onChange would avoid having an anonymous function. 
+  const onChangeHandler = (e) => {
+    const {name, value} = e.target;
+    setPostData((prevState) => {
+      return {...prevState, [name]:value}
+    });
+  }
+
     return (
       <form className="container">
         <h1>Add a New Post</h1>
-        {/* {_ Title Fields Here _}
-        {_ Quill Editor Here _} */}
+        <p>
+            <label htmlFor='form-title'>Title:</label>
+            <br />
+            <input
+              id = 'form-tile'
+              name = 'title'
+              value = {postData.title}
+              onChange = {onChangeHandler}
+            />
+        </p>
         <p>
           <button type="submit">Save</button>
         </p>
